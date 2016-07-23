3c1e7cc48f4f89da6fe3f31d23da14d8b160d1ed
Jannis Huebl <jannis.huebl@web.de>
2016-07-23 14:53:00 +0200

    activate user automaticly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,28 @@
 class UsersController < ApplicationController
-	before_action :not_login_user, only: [ :new, :create ]
-  
-	def new
-		@user = User.new
-		@signup = true
-	end
+  before_action :not_login_user, only: [ :new, :create ]
 
-	def create
-  	@user = User.new(user_params)
-  	if @user.save
-  	  flash[:success] = "#{t('.successfully')}"
-        respond_to do |format|
-          format.html { redirect_to root_path }
-          format.js
-        end
-  	else
-        respond_to do |format|
-          format.html { render 'new' }
-          format.js
-        end
-  	end
+  def new
+    @user = User.new
+    @signup = true
   end
-   def activate
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.activate!
+      flash[:success] = "#{t('.successfully')}"
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render 'new' }
+        format.js
+      end
+    end
+  end
+  def activate
     if (@user = User.load_from_activation_token(params[:id]))
       @user.activate!
       flash[:success] = "#{t('.activated')}"
@@ -33,7 +34,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,:password_confirmation)   
   end
-	def not_login_user
+  def not_login_user
     if logged_in?
       redirect_to root_path
       flash[:warning] = "#{t('users.has_already_logged_in')}"
